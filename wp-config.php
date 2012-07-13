5a1b0907da2d4bedbdf4373bc869227a61dc9116
Joe <twoguysonewok@gmail.com>
2012-07-13 19:27:40 -0400

    edit wp-config.php file to increase file upload size

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,9 @@
  * @package WordPress
  */
 
+// ** My attempt to raise the upload max file size ** //
+define('WP_MEMORY_LIMIT', '64M');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 if (isset($_SERVER["DATABASE_URL"])) {
