327acd0cf9bc4c839511706d547c8015379a16e5
Joe <twoguysonewok@gmail.com>
2012-08-15 11:47:27 -0400

    Test permalink changes

--- index.php
+++ index.php
@@ -15,3 +15,5 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require('./wp-blog-header.php');
+
+//Comment to force push to production to test permalink changes
